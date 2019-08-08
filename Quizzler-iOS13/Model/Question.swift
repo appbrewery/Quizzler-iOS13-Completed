0e068742f83b5bc5f8de50a9e97d582c408afed8
Angela <8798027+angelabauer@users.noreply.github.com>
2019-08-08 15:44:40 +0100

    Update code with comments

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 07/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
